4775cdf4d992e1d7a59b657da39c61255e83b8c2
eunsong <haley030319@gmail.com>
2023-05-01 18:18:47 +0900

    첫번쨰 커밋

---
+++ # 문자열(strings) = 문자의 나열.py
@@ -0,0 +1,20 @@
+# 문자열(strings) = 문자의 나열
+ 
+
+
+      
+      
+      
+      
+
+
+
+score = "점수:90"
+print(score.removeprefix("점수:"))
+
+score_2 = "75점"
+print(score_2.removesuffix("점"))
+
+
+city = "서울 중구"
+print(city.replace("서울", "서울시"))
